59f2b63ec1083325badde5c0444afac74f804899
Kosta Bulgakov <konstantin@oligosecurity.io>
2024-08-13 07:03:12 +0000

    adding app

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # dotnet-serilog-example
+
+## Commands
+``` bash
+dotnet new console -n SerilogTestApp
+cd SerilogTestApp
+dotnet add package Serilog
+dotnet add package Serilog.Sinks.Console
+
+dotnet build
+dotnet run --project ./SerilogTestApp/SerilogTestApp.csproj
+
+dotnet publish --configuration Release --output ./publish
+dotnet publish --configuration Release --output ./publish_self_contained --self-contained --runtime linux-x64
+```
---
+++ SerilogTestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Configure Serilog
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        while (true)
+        {
+            // Example logging
+            Log.Information("Hello, Serilog! The current time is {Time}", DateTime.Now);
+
+            // Wait for 1 second
+            Thread.Sleep(1000);
+        }
+
+        // Ensure to flush and close the logger at the end of the program (this line will not be reached due to the infinite loop)
+        // Log.CloseAndFlush();
+    }
+}
